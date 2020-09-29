0849167a0823cfbc17131b2e5e740d9b89efd38a
Ben Chan <benchan777@gmail.com>
2020-09-28 19:53:32 -0700

    add second function

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-poem_lines = {}
+lines_list = {}
 
 def get_file_lines(filename):
     with open(filename, "r") as openfile:
         line = openfile.readlines()
         line_count = 1
         for i in line:
-            poem_lines[line_count] = i
+            lines_list[line_count] = i
             line_count += 1
-
-
-# def get_file_lines(filename):
-#     with open(filename, "r") as openfile:
-#         lines_list = openfile.readlines()
-#         print(lines_list)
+    return lines_list
 
 def lines_printed_backwards(lines_list):
-    with open("poem.txt", "r") as openfile:
-        line = openfile.readlines()
-        line_count = len(line)
-
-        for i in reversed(line):
-            print(f"{line_count} {i}")
-            line_count -= 1
+    for key, value in reversed(lines_list.items()):
+        print(key, value)
 
 
 get_file_lines("poem.txt")
-print(poem_lines)
+lines_printed_backwards(lines_list)
