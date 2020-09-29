eaf7d6eaf894d7c9764ce1820753fc76ced61d86
Ben Chan <benchan777@gmail.com>
2020-09-29 01:56:58 -0700

    add ability to save to file

--- main.py
+++ main.py
@@ -37,8 +37,31 @@ def lines_printed_custom(lines_list):
     poem_string = ''.join(word_list)
     word_list_split = poem_string.split()
     sorted_list = sorted(word_list_split)
-    for i in sorted_list:
-        print(i)
+    print(sorted_list)
+
+
+def save_reverse_poem_to_file(lines_list):
+    for key, value in reversed(lines_list.items()):
+        print(f"{key}: {value}", file = open("backwards poem.txt", "a"))
+
+
+def save_random_poem_to_file(lines_list):
+    line_count = 1
+
+    while line_count <= len(lines_list):
+        index = random.randint(1, len(lines_list))
+        print(f"{index}: {lines_list[index]}", file = open("random poem.txt", "a"))
+        line_count += 1
+
+def save_alphabetical_to_file(lines_list):
+    word_list = []
+    word_list_split = []
+    for key, value in lines_list.items():
+        word_list.append(value)
+    poem_string = ''.join(word_list)
+    word_list_split = poem_string.split()
+    sorted_list = sorted(word_list_split)
+    print(sorted_list, file = open("alphabetized letters.txt", "a"))
 
 get_file_lines("poem.txt")
 print("Welcome to the poetry slam!")
@@ -63,3 +86,24 @@ while True:
         lines_printed_custom(lines_list)
         break
     print("Please only enter a number from 1-4!")
+
+print("Would you like to save this output to a file? ")
+
+while True:
+    save_file = input("Please enter yes or no: ")
+
+    if save_file == "yes" and option_select == "2":
+        save_reverse_poem_to_file(lines_list)
+        print("The poem has been saved to 'backwards poem.txt!'")
+        break
+    if save_file == "yes" and option_select == "3":
+        save_random_poem_to_file(lines_list)
+        print("The poem has been saved to 'random poem.txt!'")
+        break
+    if save_file == "yes" and option_select == "4":
+        save_alphabetical_to_file(lines_list)
+        print("The alphabetized letters have been saved to 'alphabetized letters.txt!'")
+        break
+    elif save_file == "no":
+        break
+    print("Please enter yes or no.")
